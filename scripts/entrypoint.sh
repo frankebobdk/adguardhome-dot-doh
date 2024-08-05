03324c7c036041f8342410b061028a249efc1a78
frankebobdk <50303454+frankebobdk@users.noreply.github.com>
2024-08-05 16:23:26 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -12,7 +12,7 @@ set -e
 /usr/sbin/unbound-anchor -4 -r /var/lib/unbound/root.hints -a /var/lib/unbound/root.key
 
 #Run Cloudflare DNS
-/usr/local/bin/cloudflared proxy-dns --port 5053 --upstream https://1.1.1.1/dns-query --upstream https://1.0.0.1/dns-query --upstream https://2606:4700:4700::1111/dns-query --upstream https://2606:4700:4700::1001/dns-query &
+/usr/local/bin/cloudflared proxy-dns --port 5153 --upstream https://1.1.1.1/dns-query --upstream https://1.0.0.1/dns-query --upstream https://2606:4700:4700::1111/dns-query --upstream https://2606:4700:4700::1001/dns-query &
 
 # Run Stubby
 /usr/local/bin/stubby -l &
